9abab46ddef69b218b328134672cfb73e0f3d199
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2014-03-24 19:08:36 +0000

    Lint cleanups, might have utterly destroyed code

--- elements/dash-tsdb-dataset.js
+++ elements/dash-tsdb-dataset.js
@@ -9,73 +9,84 @@ Polymer('dash-tsdb-dataset', {
                     start: "validate",
                  grouptag: "validate"
                },
-     validate: function(oldv,newv){
+     validate: function(){
 
+                 var dsi,ds,alltags;
                  for(dsi in this.spec.dss){
-                   var ds = this.spec.dss[dsi];
-                   if(ds.hasOwnProperty("rate")){
-                     ds['rate'] = false;
-                   };
-    
-                   var alltags = this.tags;
-                   if(this.spec.hasOwnProperty("grouptag")){
-                     alltags.push(this.spec.grouptag + "=*");
-                   };
-                   if(ds.hasOwnProperty("tags")){
-                     ds['tags'] = alltags.concat(ds['tags']);
-                   } else {
-                     ds['tags'] = alltags;
+                   if(this.spec.dss.hasOwnProperty(dsi)){
+                     ds = this.spec.dss[dsi];
+                     if(ds.hasOwnProperty("rate")){
+                       ds.raterate = false;
+                     }
+      
+                     alltags = this.tags;
+                     if(this.spec.hasOwnProperty("grouptag")){
+                       alltags.push(this.spec.grouptag + "=*");
+                     }
+                     if(ds.hasOwnProperty("tags")){
+                       ds.tags = alltags.concat(ds.tags);
+                     } else {
+                       ds.tags = alltags;
+                     }
                    }
-                 };
+                 }
 
                  this.specData = this.spec;
                },
   gotDsResult: function(ev){
                  var data = ev.detail;
-                 var gtag = undefined;
-                 var resmap = new Array();
+                 var gtag;
+                 var resmap = [];
                  var dssi = ev.srcElement.attributes.dsi.value;
 
                  if(this.spec.hasOwnProperty("grouptag")){
                    gtag = this.spec.grouptag;
-                 };
+                 }
 
+                 var di, t;
                  for( di in data){
-                   var t = "";
-                   if( gtag != undefined ){
-                     t = data[di].tags[gtag];
-                   };
-                   if(!resmap.hasOwnProperty(t)){
-                     resmap[t] = new Array();
-                   };
-                   resmap[t][dssi] = data[di];
-                 };
+                   if(data.hasOwnProperty(di)){
+                     t = "";
+                     if( gtag !== undefined ){
+                       t = data[di].tags[gtag];
+                     }
+                     if(!resmap.hasOwnProperty(t)){
+                       resmap[t] = [];
+                     }
+                     resmap[t][dssi] = data[di];
+                   }
+                 }
 
+                 var ri, ent,found,rs,n;
                  for(ri in resmap){
-                   var ent = new Object();
-                   ent.name = ri;
-                   ent.data = resmap[ri][dssi];
-                   var found = false;
+                   if(resmap.hasOwnProperty(ri)){
+                     ent = {};
+                     ent.name = ri;
+                     ent.data = resmap[ri][dssi];
+                     found = false;
 
-                   for( rs in this.resultset ){
-                     if(this.resultset[rs].name === ent.name){
-                       found = true;
-                       this.resultset[rs].data[dssi] = ent.data;
-                       break;
-                     };
-                   };
+                     for( rs in this.resultset ){
+                       if(this.resultset.hasOwnProperty(rs)){
+                         if(this.resultset[rs].name === ent.name){
+                           found = true;
+                           this.resultset[rs].data[dssi] = ent.data;
+                           break;
+                         }
+                       }
+                     }
 
-                   if(!found){
-                     var n = new Object();
-                     n.name = ent.name;
-                     n.data = new Array();
-                     n.data[dssi] = ent.data;
-                     this.resultset.push(n);
-                   };
-                 };
+                     if(!found){
+                       n = {};
+                       n.name = ent.name;
+                       n.data = [];
+                       n.data[dssi] = ent.data;
+                       this.resultset.push(n);
+                     }
+                   }
+                 }
                },
         ready: function(){
                  this.tags = [];
-                 this.resultset = new Array();
+                 this.resultset = [];
                }
 });
