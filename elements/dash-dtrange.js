4b884c766286084a343a95138cbfdf234361b576
Tristan Colgate <tristan@we7.com>
2014-03-30 15:37:07 +0100

    Fix feedback for updating dtrange

--- elements/dash-dtrange.js
+++ elements/dash-dtrange.js
@@ -4,7 +4,24 @@ Polymer('dash-dtrange', {
      valid: true,
    observe: {
               start_in: "validate",
-                end_in: "validate"
+                end_in: "validate",
+                 start: "update",
+                   end: "update"
+            },
+    update: function(){
+              this.start_in = this.start;
+              this.end_in = this.end;
+              if( 
+                  this.start <= this.end 
+              ){
+                this.valid = true;
+                this.$.start_input.valid = true;
+                this.$.end_input.valid = true;
+              } else {
+                this.valid = false;
+                this.$.start_input.valid = false;
+                this.$.end_input.valid = false;
+              }
             },
   validate: function(){
               if( 
