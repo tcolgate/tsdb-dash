a0faf9f10f4e5d0e1a6448432ec2e6e486a05539
Tristan Colgate <tristan@we7.com>
2014-04-07 20:55:28 +0100

    Cursory attempt with flatiron

--- elements/dash-app.js
+++ elements/dash-app.js
@@ -1,25 +1,5 @@
 Polymer('dash-app', {
-     gotTrees: function(ev,resp){
-                 this.trees = resp.response;
-               },
-    gotGroups: function(ev,resp){
-                 this.groups = resp.response;
-               },
-       router: null,
-
+        route: "test",
         ready: function () {
-                 this.trees = [];
-                 this.groups = [];
-                 setTimeout((function () {
-                   this.router = this.shadowRoot.querySelector('sr-router');
-                   this.routerReady();
-                 }).bind(this));
-               },
-
-  routerReady: function () {
-                 this.init();
-               },
- 
-         init: function () {
                }
 });
